1b4fac47ada326dabd91b6094ee0f3ec9c5128c5
Fonzy Fermin <fonzy.fermin@gmail.com>
2023-01-23 16:50:36 +0800

    Added dice class

--- Dice.cs
+++ Dice.cs
@@ -2,5 +2,17 @@
 
 public class Dice
 {
-    
+    private short diceRoll;
+
+    public short DiceRoll
+    {
+        get => diceRoll;
+        set => diceRoll = value;
+    }
+
+    private void RollDice()
+    {
+        var random = new Random();
+        diceRoll = (short) random.Next(1, 7);   // 7 because the upper bound is exclusive
+    }
 }
